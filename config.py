c53b51a2ec1b38261d958903bbbf4ac00a6108c6
Matthias Osswald <m@osswald.li>
2020-03-16 12:42:39 +0100

    Add comments how to protect cookies better.

--- config.py
+++ config.py
@@ -7,3 +7,12 @@ class Config(object):
                                            Path('.') / 'application.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
+
+    # only allow for https
+    # SESSION_COOKIE_SECURE = True
+    # secure remember_me cookie from flask_login
+    # REMEMBER_COOKIE_SECURE = True
+
+    # XSS Protection (CSRF protection already because of wtf-forms)
+    # SESSION_COOKIE_HTTPONLY = True
+    # REMEMBER_COOKIE_HTTPONLY = True
